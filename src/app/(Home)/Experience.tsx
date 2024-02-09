7b7fbfc5d710f2056ad1f4e654c1ccb691fc23db
Brandon <brandon@builtbybillt.com>
2024-02-08 19:43:22 -0600

    added mobile Experience section w/ accordion

--- src/app/(Home)/Experience.tsx
+++ src/app/(Home)/Experience.tsx
@@ -1,72 +1,62 @@
-import React from "react"
+"use client"
+import React, { useState } from "react"
+import { infoArr } from "./infoArr"
 
 function Experience() {
+	const [active, setActive] = useState("PiggyVest")
+
 	return (
-		<section className="w-full my-8">
+		<section className="w-full my-8 mb-32">
 			<h1 className="font-anton text-3xl text-right">Experience</h1>
 			<div
-				className="grid px-4 pt-12 bg-white text-slate-600 
-                            grid-cols-auto gap-12 
+				className="grid px-[4vw] pt-12 bg-white text-slate-600 rounded-lg 
+                            grid-cols-auto
                             md:grid-cols-2 md:grid-rows-5">
-				<div>
-					<div className="flex">
-						<h3>Company</h3>
-						<p>year</p>
-					</div>
-					<p>description</p>
-				</div>
-				<div>
-					<h6>roles</h6>
-					<p>description</p>
-				</div>
-
-				<div>
-					<div className="flex">
-						<h3>Company</h3>
-						<p>year</p>
-					</div>
-					<p>description</p>
-				</div>
-				<div>
-					<h6>roles</h6>
-					<p>description</p>
-				</div>
-				<div>
-					<div className="flex">
-						<h3>Company</h3>
-						<p>year</p>
-					</div>
-					<p>description</p>
-				</div>
-				<div>
-					<h6>roles</h6>
-					<p>description</p>
-				</div>
-				<div>
-					<div className="flex">
-						<h3>Company</h3>
-						<p>year</p>
-					</div>
-					<p>description</p>
-				</div>
-				<div>
-					<h6>roles</h6>
-					<p>description</p>
-				</div>
-				<div>
-					<div className="flex">
-						<h3>Company</h3>
-						<p>year</p>
-					</div>
-					<p>description</p>
-				</div>
-				<div>
-					<h6>roles</h6>
-					<p>description</p>
-				</div>
+				{infoArr.map(info => (
+					<RoleSection info={info} isActive={active === info.company} setActive={setActive} />
+				))}
 			</div>
 		</section>
 	)
 }
-
 export default Experience
+
+interface RoleSectionProps {
+	info: {
+		company: string
+		role: string
+		startEnd: string
+		description: string
+		companyBio: string
+		logoSrc: string
+	}
+	isActive: boolean
+	setActive: (company: string) => void
+}
+
+function RoleSection({ info, isActive, setActive }: RoleSectionProps) {
+	const { company, role, startEnd, description, companyBio, logoSrc } = info
+
+	return (
+		<div onClick={() => setActive(company)} className="border-b-2 pt-6 cursor-pointer">
+			<div className={`flex ${isActive ? "pb-0" : "pb-6"}  transition-all ease-linear`}>
+				<h6 className="text-xl font-bold">{company}</h6>
+				<span className="ml-2 align-self-start">{startEnd}</span>
+			</div>
+
+			<div
+				className={`flex flex-col justify-around overflow-hidden transition-all duration-500
+							${isActive ? "h-[270px] pb-6" : "h-[0px] opacity-0"}`}>
+				<h6 className="font-bold">{role}</h6>
+				<p>{description}</p>
+				<div className="flex gap-4 items-end">
+					<img src={logoSrc} alt="logo" className="basis-1/2" />
+					<div className="flex flex-col shrink basis-1/2">
+						<h6 className="font-bold">Company Bio:</h6>
+						<p>{companyBio}</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
---
+++ src/app/(Home)/infoArr.js
@@ -0,0 +1,29 @@
+export const infoArr = [
+	{
+		company: "PiggyVest",
+		role: "Front End Developer",
+		startEnd: "2021 to Present",
+		description:
+			"I founded and currently manage PiggyBest's content department, producting multiple content initiatives -- ranging from a finace blog to a savings report -- that have led to a 200% increase in organic traffic",
+		companyBio: "PiggyVest is Africa's leading savings & investmen platform with over 4.7 million users.",
+		logoSrc: "https://picsum.photos/100",
+	},
+	{
+		company: "DonkeyVest",
+		role: "Front End Developer",
+		startEnd: "2021 to Present",
+		description:
+			"I founded and currently manage PiggyBest's content department, producting multiple content initiatives -- ranging from a finace blog to a savings report -- that have led to a 200% increase in organic traffic",
+		companyBio: "PiggyVest is Africa's leading savings & investmen platform with over 4.7 million users.",
+		logoSrc: "https://picsum.photos/100",
+	},
+	{
+		company: "PiggySave",
+		role: "Front End Developer",
+		startEnd: "2021 to Present",
+		description:
+			"I founded and currently manage PiggyBest's content department, producting multiple content initiatives -- ranging from a finace blog to a savings report -- that have led to a 200% increase in organic traffic",
+		companyBio: "PiggyVest is Africa's leading savings & investmen platform with over 4.7 million users.",
+		logoSrc: "https://picsum.photos/100",
+	},
+]
