aed6bbe86ffd1fb5cd6cc0c7484e33392a2f53f3
Brandon <brandon@builtbybillt.com>
2024-02-26 11:42:27 -0600

    added bulk2save video + full width

--- src/app/(Home)/Featured.tsx
+++ src/app/(Home)/Featured.tsx
@@ -13,7 +13,7 @@ export default function Featured() {
 
 			<div className="px-1 pt-8 border-2 rounded-3xl mb-24">
 				<div className="overflow-hidden">
-					<video autoPlay muted loop className="">
+					<video autoPlay muted loop className="w-full">
 						<source src="/videos/maxie-mobile.webm" type="video/webm" media="(max-width: 767px)" />
 						<source src="/videos/maxie-mobile.mp4" type="video/mp4" media="(max-width: 767px)" />
 						<source src="/videos/maxie-loop.webm" type="video/webm" media="(min-width: 768px)" />
@@ -31,15 +31,29 @@ export default function Featured() {
 
 			<div className="px-1 pt-8 border-2 rounded-3xl mb-24">
 				<div className="overflow-hidden">
-					<video autoPlay muted loop className="">
+					<video autoPlay muted loop className="w-full">
 						<source src="/videos/dirt-e-bike-mobile.mp4" type="video/mp4" media="(max-width: 767px)" />
-						<source src="/videos/dirt-e-bike-loop.webm" type="video/webm" media="(min-width: 768px)" />
 						<source src="/videos/dirt-e-bike-loop.mp4" type="video/mp4" media="(min-width: 768px)" />
 						Your browser does not support the video tag.
 					</video>
 				</div>
 				<div className="relative">
 					<h3 className="font-anton text-3xl py-8 px-4">Dirt E-Bike</h3>
+					<div className="absolute top-0 right-4 -translate-y-1/2">
+						<CircleText text="visit live site" />
+					</div>
+				</div>
+			</div>
+			<div className="px-1 pt-8 border-2 rounded-3xl mb-24">
+				<div className="overflow-hidden">
+					<video autoPlay muted loop className="w-full">
+						<source src="/videos/bulk-to-save-mobile.mp4" type="video/mp4" media="(max-width: 767px)" />
+						<source src="/videos/bulk-to-save-loop.mp4" type="video/mp4" media="(min-width: 768px)" />
+						Your browser does not support the video tag.
+					</video>
+				</div>
+				<div className="relative">
+					<h3 className="font-anton text-3xl py-8 px-4">Bulk 2 Save</h3>
 					<div className="absolute top-0 right-4 -translate-y-1/2">
 						<CircleText text="visit live site" />
 					</div>
