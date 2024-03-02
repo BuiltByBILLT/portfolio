cfe1d4ddc8e2e29e2887228dfdfdf100018cf1b2
Brandon <brandon@builtbybillt.com>
2024-03-01 18:19:46 -0600

    added leftside of contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,8 +5,21 @@ function ContactPage() {
 	return (
 		<div className="grid grid-cols-2 mx-[4vw] mb-16 gap-8">
 			<section>
-				<h1>Contact</h1>
-				<p>Get in touch with me</p>
+				<div className="font-anton text-[15vw] leading-none">
+					<h1>WORK</h1>
+					<h1>WITH</h1>
+					<h1>ME</h1>
+				</div>
+				<div>
+					<p>Are you ready to supercharge your business with visually stunning content?</p>
+					<p>Fill out this form for a free website consultation!</p>
+					<p>
+						or email me at{" "}
+						<a className="underline" href="mailto:brandon@builtbybillt.com">
+							brandon@builtbybillt.com
+						</a>
+					</p>
+				</div>
 			</section>
 			<section className="text-xl max-w-xl">
 				<ContactForm />
