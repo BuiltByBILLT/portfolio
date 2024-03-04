df05a5713f6a949b67c1a3e00587ea1d2a0d96d9
Brandon <brandon@builtbybillt.com>
2024-03-04 10:49:56 -0600

    mobilized contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,14 +3,18 @@ import ContactForm from "@/components/ContactForm"
 
 function ContactPage() {
 	return (
-		<div className="grid grid-cols-2 mx-[4vw] mb-16 gap-8">
+		<div
+			className="grid mx-[4vw] my-8 gap-8
+		md:grid-cols-2">
 			<section>
-				<div className="font-anton text-[15vw] leading-none">
+				<div
+					className="font-anton text-center text-[30vw] leading-none
+				md:text-left md:text-[12vw]">
 					<h1>WORK</h1>
 					<h1>WITH</h1>
 					<h1>ME</h1>
 				</div>
-				<div>
+				<div className="text-center md:text-left my-6 flex flex-col gap-1">
 					<p>Are you ready to supercharge your business with visually stunning content?</p>
 					<p>Fill out this form for a free website consultation!</p>
 					<p>
