4bec8530e35acd29bb073087b77a0920eb7d476b
Brandon <brandon@builtbybillt.com>
2024-03-11 15:24:16 -0500

    added tech stack to footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,18 +1,29 @@
 import { FaLinkedin } from "react-icons/fa"
 import { RiLinkedinBoxLine } from "react-icons/ri"
+import StackIcon from "tech-stack-icons"
 
 function Footer() {
 	return (
-		<footer
-			className="bg-black px-[4vw] py-4  flex items-center 
+		<footer>
+			<section
+				className="px-[4vw] py-4  flex items-center 
        flex-col text-center gap-4
         md:flex-row md:text-left md:justify-between">
-			<div className="order-2 md:flex md:gap-2">
-				<div>Copyright &copy; 2024 Brandon Guo.</div> <div>All Rights Reserved.</div>
-			</div>
-			<a href="https://www.linkedin.com/in/brandon-guo-396aa22a7/" target="_blank" className="text-6xl md:text-4xl md:order-2">
-				<RiLinkedinBoxLine />
-			</a>
+				<div className="order-2 md:flex md:gap-2">
+					<div>Copyright &copy; 2024 Brandon Guo.</div> <div>All Rights Reserved.</div>
+				</div>
+				<a href="https://www.linkedin.com/in/brandon-guo-396aa22a7/" target="_blank" className="text-6xl md:text-4xl md:order-2">
+					<RiLinkedinBoxLine />
+				</a>
+			</section>
+			<section>
+				<div className="bg-black flex items-center justify-center gap-4 py-4">
+					Built with
+					<StackIcon name="nextjs2" className="w-8 md:w-6" />
+					<StackIcon name="typescript" className="w-8 md:w-6" />
+					<StackIcon name="tailwindcss" className="w-8 md:w-6" />
+				</div>
+			</section>
 		</footer>
 	)
 }
